88de58da233be6d541705ee86ccfd37800ba1736
alexfateev <mailboxfav@gmail.com>
2023-10-17 17:02:27 +0500

    SomeChanges

--- src/main/java/Test.java
+++ src/main/java/Test.java
@@ -1,8 +1,12 @@
-import java.text.SimpleDateFormat;
-import java.util.Date;
+import java.io.IOException;
+import java.net.ServerSocket;
 
 public class Test {
     public static void main(String[] args) {
-
+        try {
+            ServerSocket serverSocket = new ServerSocket(1234);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
---
+++ src/test/java/server/MessageTest.java
@@ -0,0 +1,12 @@
+package server;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MessageTest {
+
+    @Test
+    public void messageTest(){
+
+    }
+}
